be454303c5ab2cd06671337ff80a24f33ecb2ef1
ikun-11451 <334495606@qq.com>
2025-08-19 05:55:42 +0800

    喵喵~

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ def easter_egg():
     from colorama import init, Fore
 
     init()
-    text = "多年以后，面对AI行刑队，张三将会回想起他2023年在会议上讨论人工智能的那个下午"
+    text = "喵喵~你的麦麦被猫娘入侵了喵~"
     rainbow_colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA]
     rainbow_text = ""
     for i, char in enumerate(text):
